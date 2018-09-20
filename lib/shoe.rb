5813cc771efe2a9863c563a610b1f50eee74681d
alice T <alicewtien@gmail.com>
2018-09-20 14:42:24 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,15 +1,15 @@
 # Make your shoe class here!
 class Shoe
-  
+
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-  
+
   def initialize(brand)
     @brand = brand
   end
-  
+
   def cobble(condition)
-    if @condition == "old"
+    if condition == "old"
       puts "Your shoe is as good as new!"
     end
     self.condition = "new"
